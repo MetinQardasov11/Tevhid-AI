926a07e1180e1065bdaadfd15d353ba61d8f27e0
MetinQardasov11 <metin.qardasov2003@gmail.com>
2024-06-11 14:21:47 +0400

    Updated image url

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-app_bp = Blueprint('app', __name__, url_prefix='/', template_folder='templates', static_folder='static/uploads', static_url_path='/static/uploads')
+app_bp = Blueprint('app', __name__, url_prefix='/', template_folder='templates', static_folder='static', static_url_path='/static')
 
 
 from . import routes
